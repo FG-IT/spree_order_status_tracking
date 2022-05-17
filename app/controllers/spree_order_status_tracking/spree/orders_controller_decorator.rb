ba93294f3ca226a53d7b07d94d8beabf7519432d
QiuLin Wang <wangql.dev@gmail.com>
2022-05-17 15:18:51 -0400

    Fixed order existing check error.

--- app/controllers/spree_order_status_tracking/spree/orders_controller_decorator.rb
+++ app/controllers/spree_order_status_tracking/spree/orders_controller_decorator.rb
@@ -11,15 +11,15 @@ module SpreeOrderStatusTracking::Spree::OrdersControllerDecorator
       @order = ::Spree::Order.includes(line_items: [variant: [:option_values, :images, :product]], bill_address: :state, ship_address: :state)
                    .find_by(number: params[:order_id])
 
-      if @order.email.casecmp(params[:email]) != 0
+      if @order.present? && @order.email.casecmp(params[:email]) != 0
         @order = nil
       end
     end
 
 
     respond_to do |format|
-      if @order.nil?
-        @error = 'Sorry, no related order found.'
+      if @order.blank?
+        @error = 'Sorry, no related order found, please check whether order information is correct.'
       else
         @error = ''
       end
