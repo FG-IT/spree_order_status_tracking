8594cd591753e0a8043f8e72005f450a99a2b3cc
Josh Yan <joshyanworks@gmail.com>
2023-07-20 15:46:01 -0500

    render order status timeline

--- app/controllers/spree_order_status_tracking/spree/orders_controller_decorator.rb
+++ app/controllers/spree_order_status_tracking/spree/orders_controller_decorator.rb
@@ -1,8 +1,56 @@
 module SpreeOrderStatusTracking::Spree::OrdersControllerDecorator
 
-  def status
+  def status_timeline
+    if params[:token].blank?
+      render 'spree/orders/status'
+    else
+      @order = ::Spree::Order.includes(:shipments)
+                .where(token: params[:token]).first
 
+      if @order.completed?
+        @events = {
+          submitted: { description: "Order Submitted" }, 
+          packing: { description: "Order is being packed" },
+          partial_shipped: { description: "Package Partially Shipped" },
+          shipped: { description: "Package Shipped" },
+          in_transit: { description: "In Transit" },
+          delivered: { description: "Package Delivered" }
+        }
+
+        @events[:submitted][:record_time] = @order.completed_at
+
+        # if @order.paid?
+        #   @events[:paid][:record_time] = @order.approved_at
+        # end
+
+        if @order.approved?
+          @events[:packing][:record_time] = @order.approved_at
+        end
+
+        if @order.shipped?
+          shipment_items = []
+          shipped_times = []
+          @order.shipments.each do |shipment|
+            shipment_items << {tracking: shipment.tracking, shipped_at: shipment.shipped_at, carrier: shipment.carrier, state: shipment.state}
+            shipped_times << shipment.shipped_at if shipment.shipped_at.present?
+          end
+
+          if @order.shipment_state == "partial"
+            @events[:partial_shipped][:items] = shipment_items
+            @events[:partial_shipped][:record_time] = shipped_times.sort.first
+          elsif @order.shipment_state == "shipped"
+            @events[:shipped][:items] = shipment_items
+            @events[:shipped][:record_time] = shipped_times.sort.last
+          end
+        end
+
+        render 'spree/orders/status_timeline'
+      else 
+        redirect_to spree.cart_path
+      end
+    end
   end
+
 
   def status_check
     if params[:email].blank?
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Spree::Core::Engine.add_routes do
-  get '/order-status' => 'orders#status', as: :order_status_page
+  get '/order-status' => 'orders#status_timeline', as: :order_status_page
   post '/order-status' => 'orders#status_check'
 end
